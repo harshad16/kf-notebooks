6e15fd9160432cd15bdec5658f8bb99926e929b2
Harshad Reddy Nalla <hnalla@redhat.com>
2025-07-28 15:17:09 -0400

    UI Fixes
    Signed-off-by: Harshad Reddy Nalla <hnalla@redhat.com>

--- workspaces/frontend/config/webpack.common.js
+++ workspaces/frontend/config/webpack.common.js
@@ -6,7 +6,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const ASSET_PATH = process.env.ASSET_PATH || '/';
+const ASSET_PATH = process.env.ASSET_PATH || '/notebooks/';
 const IMAGES_DIRNAME = 'images';
 const relativeDir = path.resolve(__dirname, '..');
 module.exports = (env) => {
